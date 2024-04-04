7248e29b5c891acee0e5608df05ca42c32d4c981
Timon0219 <andreslopez0219.github@gmail.com>
2024-04-04 11:14:02 -0400

    chore: clear form functionality

--- src/components/simpleform.js
+++ src/components/simpleform.js
@@ -1,8 +1,13 @@
-const SimpleForm = ({ title }) => {
+const SimpleForm = ({ title, name, formData, setFormData }) => {
+  const handleChange = (e) => {
+    setFormData({ ...formData, name: e.target.value });
+  };
   return (
     <div className="flex flex-col gap-3 w-full p-6 bg-white border border-slate-300 rounded-lg">
       <h2 className="text-left">{title}</h2>
       <input
+        name={name}
+        onChange={(e) => handleChange(e)}
         placeholder="Your answser"
         className="py-2 max-w-[50%] border-b-2 focus:outline-none focus:border-b-[black] duration-200"
       />
--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,17 +1,64 @@
+import { useState } from "react";
 import HeaderForm from "../../components/headerform";
 import SimpleForm from "../../components/simpleform";
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    industry: "",
+    service: "",
+    phone: "",
+  });
+  const handleSubmit = () => {
+    setFormData({ name: "", email: "", industry: "", service: "", phone: "" });
+  };
   return (
     <div className="flex justify-center w-full h-full px-4 py-4 bg-slate-200">
-      <form className="flex flex-col gap-4 w-[400px] md:w-[600px]">
+      <form
+        className="flex flex-col gap-4 w-[400px] md:w-[600px]"
+        onSubmit={() => handleSubmit()}
+      >
         <HeaderForm />
-        <SimpleForm title="Name" />
-        <SimpleForm title="Industry" />
-        <SimpleForm title="What service or team do you want?" />
-        <SimpleForm title="Email" />
-        <SimpleForm title="Phone Number" />
+        <SimpleForm
+          title="Name"
+          name="name"
+          value={formData.name}
+          formData={formData}
+          setFormData={setFormData}
+        />
+        <SimpleForm
+          title="Industry"
+          name="industry"
+          value={formData.industry}
+          formData={formData}
+          setFormData={setFormData}
+        />
+        <SimpleForm
+          title="What service or team do you want?"
+          name="service"
+          value={formData.service}
+          formData={formData}
+          setFormData={setFormData}
+        />
+        <SimpleForm
+          title="Email"
+          name="email"
+          value={formData.email}
+          formData={formData}
+          setFormData={setFormData}
+        />
+        <SimpleForm
+          title="Phone Number"
+          name="phone"
+          value={formData.phone}
+          formData={formData}
+          setFormData={setFormData}
+        />
         <div className="flex justify-between w-full">
-          <button className="px-6 py-2 bg-black hover:bg-white text-white font-semibold hover:text-black rounded-lg duration-200">
+          <button
+            type="submit"
+            className="px-6 py-2 bg-black hover:bg-white text-white font-semibold hover:text-black rounded-lg duration-200"
+          >
             Submit
           </button>
           <button className="px-6 py-2  font-semibold hover:text-white hover:bg-black rounded-lg duration-200">
